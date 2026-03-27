6e1e96360ac15acc26210ba6b0f11369c703c0a2
thailandanshu-coder <thailandanshu@gmail.com>
2026-03-28 02:42:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, request
-
+import subprocess
 import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route('/')
 def home():
     return '''
-    <h2>APK Login Remover Tool</h2>
-    <form action="/run" method="post">
-        <button type="submit">Run Tool</button>
+    <h2>APK Login Remover</h2>
+    <form action="/run" method="post" enctype="multipart/form-data">
+        <input type="file" name="apk_file" required>
+        <button type="submit">Upload & Run</button>
     </form>
     '''
-import subprocess
 
 @app.route('/run', methods=['POST'])
 def run_tool():
+    file = request.files['apk_file']
+
+    if file.filename == '':
+        return "No file selected ❌"
+
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+
     result = subprocess.run(
-        ["python3", "apk_login_remover.py"],
+        ["python3", "apk_login_remover.py", filepath],
         capture_output=True,
         text=True
     )
@@ -26,7 +37,7 @@ def run_tool():
     <h3>Output:</h3>
     <pre>{result.stdout}</pre>
 
-    <h3>Error (if any):</h3>
+    <h3>Error:</h3>
     <pre>{result.stderr}</pre>
     """
 
